e0d5bc2f3d40f8933432ad53daf57a17542296c2
lumiknit <aasr4r4@gmail.com>
2020-11-01 13:43:53 +0000

    First commit

---
+++ ize.lua
@@ -0,0 +1,17 @@
+#!/bin/sh
+_=[[
+IFS=:
+for D in ${PATH}; do
+  for F in "${D}"/lua "${D}"/lua5* "${D}"/luajit*; do
+    if [ -x "${F}"  ]; then
+      exec "${F}" "$0" "$@"
+    fi
+  done
+done
+printf "%s: no Lua interpreter found\n" "${0##*/}" >&2
+exit 1
+]]
+
+-- ize.lua 0.0.1d
+-- lumiknit (aasr4r4@gmail.com)
+
